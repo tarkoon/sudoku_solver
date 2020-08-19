1c458b5328211ca9baf6e7b592dd50eecd2e2e57
Tristan Egan <tristan_egan@yahoo.com>
2020-08-18 20:21:41 -0500

    Refactored Code

--- sudoku_gui.py
+++ sudoku_gui.py
@@ -1,32 +1,33 @@
 import pygame
 from math import sqrt
 
-pygame.init()
+pygame.font.init()
 
 BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
+GREY = (50, 50, 50)
+WHITE = (250, 250, 250)
 GREEN = (50, 255, 100)
 BLUE = (0, 0, 255)
 
-board = [
-    [8, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 3, 6, 0, 0, 0, 0, 0],
-    [0, 7, 0, 0, 9, 0, 2, 0, 0],
-    [0, 5, 0, 0, 0, 7, 0, 0, 0],
-    [0, 0, 0, 0, 4, 5, 7, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0, 3, 0],
-    [0, 0, 1, 0, 0, 0, 0, 6, 8],
-    [0, 0, 8, 5, 0, 0, 0, 1, 0],
-    [0, 9, 0, 0, 0, 0, 4, 0, 0],
+sudoku_board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9],
 ]
 
 
 class Cell:
     def __init__(self, value, position):
+        self.color = GREY
         self.value = value
-        self.position = position
         self.size = (56, 56)
-        self.color = BLACK
+        self.position = self.x, self.y = position
         self.font = pygame.font.SysFont("comicsans", 50)
         self.rect = pygame.Rect(self.position, self.size)
 
@@ -35,22 +36,70 @@ class Cell:
         pygame.draw.rect(window, self.color, self.rect, 1)
 
         if self.value != 0:
-            text = self.font.render(str(self.value), 1, BLUE)
-            window.blit(text, self.position)
+            text = self.font.render(str(self.value), 1, self.color)
+            window.blit(
+                text, ((self.x + text.get_width()), (self.y + text.get_height() // 3)),
+            )
 
 
 class Grid:
-    def __init__(self, board):
-        self.cell_list = [
-            [
-                Cell(board[row][col], (col * 56 + 48, row * 56 + 48))
-                for col in range(len(board))
-            ]
-            for row in range(len(board))
-        ]
+    def __init__(self, window, width, height, board, instant):
+        self.window = window
+        self.width = width
+        self.height = height
         self.board = board
 
-    def solve(self, window, width, height):
+        self.side_buffer = 48
+        self.wait_time = 50
+        if instant:
+            self.wait_time = 0
+
+        self.cells = [
+            [
+                Cell(board[row][col], (col * 56 + 48, row * 56 + 48))
+                for col in range(len(self.board))
+            ]
+            for row in range(len(self.board))
+        ]
+
+    def draw_grid(self):
+        for row in self.cells:
+            for cell in row:
+                cell.draw(self.window)
+        self.draw_lines()
+
+    def draw_lines(self):
+
+        x = y = self.side_buffer
+
+        for row in range(len(self.board)):
+            for col in range(len(self.board)):
+
+                if col % 3 == 0 and col != 0:
+                    pygame.draw.line(
+                        self.window,
+                        BLACK,
+                        (x, y),
+                        (x, self.height - self.side_buffer),
+                        5,
+                    )
+
+                x += 56
+
+            x = self.side_buffer
+            y += 56
+
+            if (row + 1) % 3 == 0 and row != 8:
+                pygame.draw.line(
+                    self.window, BLACK, (x, y), (self.width - self.side_buffer, y), 5
+                )
+
+    def solve(self):
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
 
         if not (empty_position := self.check_board()):
             return True
@@ -60,27 +109,29 @@ class Grid:
         for num in range(1, 10):
             if self.is_valid(num, empty_position):
                 self.board[x][y] = num
-                self.cell_list[x][y].value = num
-                self.cell_list[x][y].draw(window)
-                draw_gird(window, width, height)
-                pygame.display.update()
-                pygame.time.delay(100)
+                self.cells[x][y].value = num
 
-                if self.solve(window, width, height):
+                self.draw_grid()
+
+                pygame.display.update()
+                pygame.time.delay(self.wait_time)
+
+                if self.solve():
                     return True
 
                 self.board[x][y] = 0
-                self.cell_list[x][y].value = 0
-                self.cell_list[x][y].draw(window)
-                draw_gird(window, width, height)
+                self.cells[x][y].value = 0
+
+                self.draw_grid()
+
                 pygame.display.update()
-                pygame.time.delay(100)
+                pygame.time.delay(self.wait_time)
 
         return False
 
     def is_valid(self, value, position):
         x, y = position
-        max_length = len(board)
+        max_length = len(self.board)
 
         for num in range(len(self.board)):
             if (self.board[x][num] == value and y != num) or (
@@ -93,7 +144,7 @@ class Grid:
 
         for row in range(box_x * 3, box_x * 3 + 3):
             for col in range(box_y * 3, box_y * 3 + 3):
-                if board[row][col] == value and (row, col) != position:
+                if self.board[row][col] == value and (row, col) != position:
                     return False
 
         return True
@@ -106,59 +157,31 @@ class Grid:
         return None
 
 
-def draw_gird(window, width, height):
-
-    x = 48
-    y = 48
-
-    for row in range(9):
-        for col in range(9):
-
-            if col % 3 == 0 and col != 0:
-                pygame.draw.line(window, BLACK, (x, y), (x, height - 48), 5)
-
-            x += 56
-
-        x = 48
-        y += 56
-
-        if (row + 1) % 3 == 0 and row != 8:
-            pygame.draw.line(window, BLACK, (x, y), (width - 48, y), 5)
-
-
 class SudokuSolver:
-    def __init__(self, width, height):
+    def __init__(self, width, height, instant=False):
         self.size = self.width, self.height = width, height
         self.window = pygame.display.set_mode(self.size)
         self.bk_color = WHITE
-        self.clock = pygame.time.Clock()
-        self.grid = Grid(board)
+        self.grid = Grid(self.window, self.width, self.height, sudoku_board, instant)
 
         self.run()
 
     def run(self):
         while True:
-            # self.clock.tick(60)
-            self.event_loop()
             self.window.fill(self.bk_color)
 
-            for row in self.grid.cell_list:
-                for cell in row:
-                    cell.draw(self.window)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.grid.solve()
 
-            draw_gird(self.window, self.width, self.height)
+            self.grid.draw_grid()
 
             pygame.display.update()
 
-    def event_loop(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    self.grid.solve(self.window, self.width, self.height)
-
 
 if __name__ == "__main__":
-    SudokuSolver(600, 600)
+    SudokuSolver(600, 600, instant=True)
